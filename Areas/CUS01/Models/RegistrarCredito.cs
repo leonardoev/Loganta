2ebcf32dcf92baf8b4c9b66b1cf8a17ef6dd7629
leonardoev <leonardoev@gmail.com>
2021-10-12 16:47:41 -0500

    front

---
+++ Areas/CUS01/Models/RegistrarCredito.cs
@@ -0,0 +1,14 @@
+﻿using loganta.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace loganta.Areas.CUS01.Models
+{
+    public class RegistrarCredito
+    {
+        public Proyecto Proyecto { get; set; }
+        public Requerimiento Requerimiento { get; set; }
+    }
+}
